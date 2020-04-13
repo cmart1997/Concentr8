c5af7214e735979905c7d2b58518a67b205d7efb
cmart1997 <christmartinez1997@yahoo.com>
2020-04-12 23:47:50 -0700

    added sessions_dictionary
    sessions dictionary added in order to keep track of the scores recorded for each session user initiated

--- app/routes.py
+++ app/routes.py
@@ -1,19 +1,29 @@
 from app import app
 from app.counter import *
 from flask import Flask, flash, redirect, render_template, request, url_for
+from datetime import datetime
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-counter = 0
+counter = 0 
+previous_sessions = {}
 @app.route('/click',methods=['GET', 'POST'])
 def count():
     global counter
+    global previous_session
     if request.method == 'POST':
         counter = counter + 1
-    
     else: 
+        today = datetime.date(datetime.now())
+        today = str(today)
+        if str(datetime.date(datetime.now())) not in previous_sessions: 
+            previous_sessions[today] = []
+            
+        else: 
+            previous_sessions[today].append(counter)
+        
         counter = 0
-    
-    return render_template('click.html', counter = counter)
+
+    return render_template('click.html', counter = counter, previous_sessions = previous_sessions)
